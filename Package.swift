860e82efaf261da37483a5f51555be83e5a79ad3
Davide De Rosa <keeshux@gmail.com>
2024-07-14 15:33:48 +0000

    0.0.20240714

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "wg-go",
-            url: "https://github.com/passepartoutvpn/wg-go-apple/releases/download/0.0.20240326/wg-go.xcframework.zip",
-            checksum: "d986525d1ed93c9c90245723f5afec93c9c60e8b1c475aca33377de3c6566d57"
+            url: "https://github.com/passepartoutvpn/wg-go-apple/releases/download/0.0.20240714/wg-go.xcframework.zip",
+            checksum: "4c2a63b15e73a966260ba86d2c95eefdf0697843252e83bc4960babece04eabf"
         )
 
 //        // local development
